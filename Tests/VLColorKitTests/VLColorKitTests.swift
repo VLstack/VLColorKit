bb67def952adebd8dc2d95bc4867b6e50b7db489
VLstack <github@vlstack.fr>
2024-02-23 08:03:27 +0100

    Initial Commit

---
+++ Tests/VLColorKitTests/VLColorKitTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import VLColorKit
+
+final class VLColorKitTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
