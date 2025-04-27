d44206cae7d8cd518d85cf27b5d1613c42520511
VLstack <github@vlstack.fr>
2025-04-27 23:30:43 +0200

    rework luminace logic

--- Sources/VLColorKit/Color.swift
+++ Sources/VLColorKit/Color.swift
@@ -1,39 +1,25 @@
 import SwiftUI
 import UIKit
 
-public extension Color
+extension Color
 {
- /// Initializes a color object from a hexadecimal string representation.
- ///
- /// - Parameter hex: A hexadecimal string representing the color.
- init(hex: String,
-      fallback: UIColor = .black)
+ public init(hex: String,
+             fallback: UIColor = .black)
  {
   self.init(uiColor: UIColor(hex: hex) ?? fallback)
  }
 
- func toHex(prefixed: Bool = false,
-            includeAlpha: Bool = false) -> String?
+ public func toHex(prefixed: Bool = false,
+                   includeAlpha: Bool = false,
+                   fallback: String = "000000") -> String
  {
   UIColor(self).toHex(prefixed: prefixed,
-                      includeAlpha: includeAlpha)
+                      includeAlpha: includeAlpha,
+                      fallback: fallback)
  }
 
- /// Determines if the color is dark or light.
- ///
- /// - Returns: `true` if the color is considered dark, `false` otherwise.
- var isDarkColor: Bool
+ public var isDark: Bool
  {
-  UIColor(self).isDarkColor
- }
- 
- /// Provides a contrasting color based on the current color's brightness.
- ///
- /// If the current color is dark, returns a light color. If the current color is light, returns a dark color.
- ///
- /// - Returns: A contrasting color based on the brightness of the current color.
- var contrastingColor: Color
- {
-  Color(uiColor: UIColor(self).contrastingColor)
+  UIColor(self).isDark
  }
 }
--- Sources/VLColorKit/UIColor.swift
+++ Sources/VLColorKit/UIColor.swift
@@ -1,8 +1,8 @@
 import UIKit
 
-public extension UIColor
+extension UIColor
 {
- convenience init?(hex: String)
+ public convenience init?(hex: String)
  {
   var hexString = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
   if hexString.hasPrefix("#") { hexString.removeFirst() }
@@ -34,31 +34,58 @@ public extension UIColor
   self.init(red: CGFloat(red) / 255, green: CGFloat(green) / 255, blue: CGFloat(blue) / 255, alpha: CGFloat(alpha) / 255)
  }
 
- /// Provides a contrasting color based on the current color's brightness.
- ///
- /// - Returns: A contrasting color (`UIColor.white` or `UIColor.black`) based on the brightness of the current color.
- var contrastingColor: UIColor
+ public var relativeLuminance: CGFloat
  {
-  self.isDarkColor ? .white : .black
+  var r: CGFloat = 0, g: CGFloat = 0, b: CGFloat = 0, a: CGFloat = 0
+  getRed(&r, green: &g, blue: &b, alpha: &a)
+
+  func adjust(_ value: CGFloat) -> CGFloat
+  {
+   return (value <= 0.03928) ? (value / 12.92) : pow((value + 0.055) / 1.055, 2.4)
+  }
+
+  let rl = adjust(r)
+  let gl = adjust(g)
+  let bl = adjust(b)
+
+  return 0.2126 * rl + 0.7152 * gl + 0.0722 * bl
  }
 
- /// Determines if the color is dark or light based on its luminance.
- ///
- /// - Returns: `true` if the color is considered dark, `false` otherwise.
- var isDarkColor: Bool
+ public func contrastRatio(with other: UIColor) -> CGFloat
  {
-  var r, g, b, a: CGFloat
-  (r, g, b, a) = (0, 0, 0, 0)
-  self.getRed(&r, green: &g, blue: &b, alpha: &a)
-  let lum = 0.2126 * r + 0.7152 * g + 0.0722 * b
+  let l1 = self.relativeLuminance
+  let l2 = other.relativeLuminance
 
-  return lum < 0.5
+  return (max(l1, l2) + 0.05) / (min(l1, l2) + 0.05)
  }
 
- func toHex(prefixed: Bool = false,
-            includeAlpha: Bool = false) -> String?
+ public var isDark: Bool
  {
-  guard let components = self.cgColor.components else { return nil }
+  let black = UIColor.black
+  let white = UIColor.white
+
+  let contrastWithBlack = self.contrastRatio(with: black)
+  let contrastWithWhite = self.contrastRatio(with: white)
+
+  if contrastWithBlack >= 7.0
+  {
+   return false
+  }
+
+  if contrastWithWhite >= 7.0
+  {
+   return true
+  }
+
+  return (contrastWithBlack > contrastWithWhite) ? false : true
+ }
+
+ public func toHex(prefixed: Bool = false,
+                   includeAlpha: Bool = false,
+                   fallback: String = "000000") -> String
+ {
+  guard let components = self.cgColor.components
+  else { return prefixed ? "#" + fallback : fallback }
 
   let red = Int(components[0] * 255.0)
   let green = Int(components[1] * 255.0)
